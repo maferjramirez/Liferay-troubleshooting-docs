69450b2f635481f1655e07742f99218a5faf777e
Martin Jimenez <34499879+maferjramirez@users.noreply.github.com>
2023-03-10 13:04:02 -0600

    Create lazy-load-images.js

---
+++ lazy-load-images.js
@@ -0,0 +1,50 @@
+AUI().use(
+	'aui-base',
+	function(A) {
+		var WIN = A.getWin();
+
+		var lazyLoadNode = A.all('.lazy-load');
+
+		var lazyLoad = function() {
+			var currentScrollPos = WIN.get('docScrollY');
+
+			var winHeight = WIN.get('innerHeight');
+
+			if (winHeight == undefined) {
+				winHeight = document.documentElement.clientHeight;
+			}
+
+			lazyLoadNode.each(
+				function(item, index, collection) {
+					if (!item.hasClass('lazy-loaded')) {
+						var loadPos = item.getY() - winHeight;
+
+						var dataOffset = parseInt(item.attr('data-offset'));
+
+						if (dataOffset) {
+							loadPos = loadPos + dataOffset;
+						}
+
+						if (currentScrollPos > loadPos) {
+							var datasrc = item.attr('datasrc');
+							var src = item.attr('src');
+
+							if (src != datasrc) {
+								item.attr('src', datasrc);
+							}
+
+							item.addClass('lazy-loaded');
+						}
+					}
+				}
+			);
+		};
+
+		if (!lazyLoadNode.isEmpty()) {
+			A.on('scroll', lazyLoad);
+			A.on('resize', lazyLoad);
+
+			lazyLoad();
+		}
+	}
+);
